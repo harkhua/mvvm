8c27ff3af52ab6a3f8b6a9d5937803bbaffbbdee
饶志华 <879467666@qq.com>
2020-01-07 09:54:51 +0800

    Add files via upload

---
+++ ts/dep.d.ts
@@ -0,0 +1,81 @@
+/**
+ * 初始化
+ * 获取节点下整个dom树 --> 解析dom树 并生成对应的vNode --> 解析对应vNode
+ * 解析待解析指令 执行update 更新到对应节点
+ *  注：不渲染整个dom树 而是做局部渲染 降低性能消耗
+ *  注：for指令会增加 dom节点
+ *  注：如果for指令中 localData中数据名和static data中数据名一致时 优先使用 localData中对应数据
+ */
+
+interface vNode {
+    id: number; // 虚拟节点编号   for指令执行update 可改变节点编号 
+    attr?: {[key: string]: string}; // 解析完之后标签属性
+    dir?: {[key: string]: string}; // 待解析指令 key 数据路径 value 指令String eg：data.a.b: text|show|bind
+    /**
+     * 根据数据路径 拿到对应虚拟节点
+     * 根据虚拟节点 拿到对应 数据路径对应的dir
+     * 通过dir下update函数 进行视图更新
+     */
+    name: string; // 节点名
+    text?: string; // 文本节点 | 注释节点 才存在
+    dep?: {[key: string]: dirExpr[]}; // 解析后的数据路径 对应依赖的指令
+    localData?: any; // component | for指令 解析后缓存数据
+    isForDir?: boolean; // 节点上是否 存在for指令
+    el?: Node; // 非For指令 原始Dom | for指令下的复制Dom
+    __el?: Node; // for指令下 才存在 缓存原始节点
+    children?: vNode[]; // 子节点
+    parent?: vNode;
+    _idx?: number;
+    noParse?: boolean; // 带有该标志不解析成 HTMLElement
+    channel?: string; // 模块渠道
+    __forItemVns__?: vNode[];
+    forEndVn?: vNode;
+    oldDisplayVal?: string;
+}
+interface textExprVal {
+    val: string;
+    expr: boolean;
+}
+interface dirExpr {
+    item?: string;
+    index?: string; 
+    value?: string|textExprVal[];
+    expr?: string;
+}
+interface dataDepNodeMap {
+    [key: string]: vNode[];
+}
+interface baseCfg {
+    el?: string;
+    template?: string;
+    mount?: string;
+}
+type anyMap = {[key: string]: any};
+interface mvvmCfg extends baseCfg {
+    data: anyMap | (() => anyMap);
+    computed: anyMap;
+    methods: {
+        [key: string]: (...args) => void;
+    };
+    watch: {
+        [key: string]: (newVal, oldVal) => void;
+    }
+    // 生命周期钩子
+    beforeCreate?: () => void;
+    created?: () => void;
+    beforeMount?: () => void;
+    mounted?: () => void;
+    beforeUpdate?: () => void;
+    updated?: () => void;
+}
+
+interface EventTarget {
+    attachEvent(type: string, listener: EventListenerOrEventListenerObject | null): void;
+}
+interface watchFn {
+    (newVal: any, oldVal: any): void
+}
+interface methodArgs {
+    method: string;
+    args: Array<string>;
+}
---
+++ ts/main.ts
@@ -0,0 +1,142 @@
+import { Store } from "./assist/store";
+import { channel } from "./assist/ini";
+import { getMvvmId, Dom, Vnode } from "./assist/helper";
+import { isFunction, setData, uniqueVn, readData, nextTick } from "./assist/util";
+import { parseVNode } from "./parse/vNode";
+import * as Dir from "./parse/dir";
+/**
+ * 处理数据监听事件
+ * @param channel 
+ * @param watchs 
+ */
+function processorWatchs (mvvm: Mvvm, watchs: {[key: string]: watchFn}){
+    Store.save(mvvm.channel, 'watchs', watchs);
+    let fn;
+    for (let name in watchs){
+        fn = function (...args){
+            watchs[name].apply(mvvm, args);
+        }
+        Store.on(name, fn);
+    }
+}
+function processorMethods (
+    data: anyMap, 
+    methods: {[key: string]: (...args) => void}
+) {
+    const _methods = {};
+    for(let name in methods) {
+        _methods[name] = function (...args) {
+            methods[name].apply(data, args);
+        }
+    }
+    return _methods;
+}
+export class Mvvm {
+    channel: string; // 类缓存渠道
+    mount: HTMLElement;
+    template: string;
+    vNode: vNode;
+    constructor (opts: mvvmCfg){
+        // 初始化前
+        opts.beforeCreate && opts.beforeCreate.call(this);
+        // 初始化
+        const data = isFunction(opts.data) ? 
+            (opts.data as () => anyMap)() : 
+            opts.data as anyMap;
+
+        const mChannel = this.channel = `${channel}|${getMvvmId()}`;
+        // 缓存数据
+        Store.save(mChannel, 'data', data || {});
+        // 缓存实例类
+        Store.save(mChannel, 'mvvm', this);
+        // 缓存数据path对应vnode
+        Store.save(mChannel, 'dataPathToVn', {});
+        // 缓存事件
+        opts.methods && Store.save(
+            mChannel,
+            'methods',
+            processorMethods(data, opts.methods)
+        );
+        // 缓存computed
+        opts.computed && Store.save(mChannel, 'computed', opts.computed);
+        // 处理数据监听事件
+        processorWatchs(this, opts.watch || {});
+        // 初始化完成后
+        opts.created && opts.created.call(this);
+
+        // 解析节点
+        let tpl = opts.template;
+        let mount = opts.mount && Dom.getElment(opts.mount)[0];
+        if (opts.el) {
+            mount = this.mount = Dom.getElment(opts.el)[0];
+            if (mount) {
+                tpl = mount.innerHTML;
+                Dom.empty(mount);
+            }
+        }
+
+        if (!tpl) {
+            return;
+        }
+        this.template = tpl;
+        const vns = this.vNode = parseVNode(tpl, mChannel);
+        vns.el = mount;
+        // 处理指令
+        Dir.parse(vns.children);
+        // 挂载回节点
+        mount.appendChild(Vnode.toDom(vns));
+        // 处理nextTick
+        // nextTick(() => {})
+    }
+    /**
+     * 设置数据
+     * @param path 
+     * @param data 
+     */
+    set (path: string, data?){
+        /**
+         * 1、通过路径获取对应虚拟节点
+         * 2、通过id去除重复虚拟节点
+         * 3、获取解析指令
+         * 4、重新解析指令后调用 update函数
+         * 5、更新数据
+         */
+        const mChannel = this.channel
+        const oData = Store.read(mChannel, 'data');
+        nextTick(function (){
+            Store.emit(path, data, readData(oData, path))
+        })
+        setData(oData, data, path);
+
+        const pathToVn = Store.read(mChannel, 'dataPathToVn');
+        let depVns = [] as vNode[];
+        depVns = pathToVn[path] || [];
+        if (!depVns.length) {
+            for (let _path in pathToVn) {
+                if (_path.indexOf(path) === 0) {
+                    depVns = depVns.concat(pathToVn[_path]);
+                }
+            }
+        }
+        uniqueVn(depVns).forEach(vn => {
+            for (let dirName in vn.dep){
+                Dir[dirName].update(vn, data);
+            }
+        });
+    }
+    /**
+     * 获取数据
+     * @param path 
+     */
+    get (path: string){
+        return readData(
+            Store.read(this.channel, 'data'), 
+            path
+        );
+    }
+    watch (path: string, cb: watchFn) {
+        Store.on(path, (...args) => {
+            cb.apply(this, args);
+        });
+    }
+}
